c9cfe742a5c7d4f7a0713ade78f1c971643c6902
FatimaZhraMardi <163603735+FatimaZhraMardi@users.noreply.github.com>
2024-03-16 13:46:43 +0000

    bdd

---
+++ inscription_form.php
@@ -0,0 +1,8 @@
+<fieldset class="border">
+    <legend class="sous_border" >Votre entreprise</legend>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <!-- Le reste du formulaire d'inscription -->
+        <!-- ... -->
+        <input class="valide" type="submit" value="Valider">
+    </form>
+</fieldset>
